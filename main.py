b2c8d28cffad9bd10f128854af953344ceccd95a
Felix Jose Contreras Aldana <f3lixj21@gmail.com>
2023-06-24 19:00:12 -0500

    up10

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-
-
 def index():
     return {"Mensaje" : "Hola Mundo"}
 
+@app.get("/validar/{nuemro}")
+def validar_capicua(numero:str):
+    respuesta = 'No es capícua'
+    
+    if numero == numero[::-1]:
+        respuesta = 'Si es capicua'
+        
+    return {
+        'numero': numero,
+        'validación':respuesta
+    }
